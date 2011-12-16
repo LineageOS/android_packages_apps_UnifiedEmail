96aff3039241bbe0f7b58a0cf47e85508fce176d
Mindy Pereira <mindyp@google.com>
2011-12-16 13:07:49 -0800

    Use chips static resources.
    Change-Id: I97f6de7c2b92a853642883442b72758bb11edf47

--- Android.mk
+++ Android.mk
@@ -20,5 +20,6 @@ LOCAL_SRC_FILES := $(call all-java-files-under, $(src_dirs)) \
         $(call all-logtags-files-under, $(src_dirs))
 LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, $(res_dirs)) $(LOCAL_PATH)/res
 LOCAL_AAPT_FLAGS := --auto-add-overlay
+LOCAL_AAPT_FLAGS += --extra-packages com.android.ex.chips
 
 include $(BUILD_PACKAGE)
