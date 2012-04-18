8520fa3ff87e28a7c24c5b09c462aa444e8956af
Marc Blank <mblank@google.com>
2012-04-18 16:07:45 -0700

    Swap in a null cursor before clearing the ConversationList adapter
    Change-Id: I41cbf2bcefe9ba2021f7eb00c43700ea1e48f564

--- src/com/android/mail/ui/ConversationListFragment.java
+++ src/com/android/mail/ui/ConversationListFragment.java
@@ -25,7 +25,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Parcelable;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewGroup.MarginLayoutParams;
@@ -296,7 +295,8 @@ public final class ConversationListFragment extends ListFragment implements
     public void onDestroyView() {
         mListSavedState = mListView.onSaveInstanceState();
 
-        // Clear the adapter.
+        // Set a null cursor in the dapter, and clear the list's adapter
+        mListAdapter.swapCursor(null);
         mListView.setAdapter(null);
 
         mActivity.unsetViewModeListener(this);
