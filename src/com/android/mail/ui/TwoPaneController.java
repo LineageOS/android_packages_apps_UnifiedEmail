907e0d8e10457f535b47b9849873f4ab9bec72ad
Andrew Sapperstein <asapperstein@google.com>
2012-06-29 14:54:34 -0700

    Fixed two pane bug that caused undo to do nothing.
    Change-Id: Ic3819d6f355755dec5a95a6664faebfb856c5da6

--- src/com/android/mail/ui/TwoPaneController.java
+++ src/com/android/mail/ui/TwoPaneController.java
@@ -437,7 +437,7 @@ public final class TwoPaneController extends AbstractActivityController {
                 }
                 mUndoBarView.setLayoutParams(params);
                 mUndoBarView.show(true, mActivity.getActivityContext(), op, mAccount,
-                        convList.getAnimatedAdapter(), null);
+                        convList.getAnimatedAdapter(), mConversationListCursor);
                 break;
         }
     }
