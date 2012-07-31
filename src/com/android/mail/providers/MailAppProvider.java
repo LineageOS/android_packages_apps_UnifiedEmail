707f24c693f116901660ce4b07598b474e681319
Vikram Aggarwal <viki@google.com>
2012-07-31 15:59:36 -0700

    Put Priority Arrows enabled/disabled in Settings
    No user-visible change yet. The next step is to read the setting and
    change the Conversation List and Conversation View to remove the
    priority inbox arrows.
    
    Change-Id: I44b644f1206f20f75891f7e0870fb3b72e56ab15

--- src/com/android/mail/providers/MailAppProvider.java
+++ src/com/android/mail/providers/MailAppProvider.java
@@ -283,6 +283,9 @@ public abstract class MailAppProvider extends ContentProvider
                 } else if (TextUtils.equals(column,
                         UIProvider.AccountColumns.SettingsColumns.SWIPE)) {
                     builder.add(account.settings.swipe);
+                } else if (TextUtils.equals(column,
+                        UIProvider.AccountColumns.SettingsColumns.PRIORITY_ARROWS_ENABLED)) {
+                    builder.add(Integer.valueOf(account.settings.priorityArrowsEnabled ? 1 : 0));
                 } else {
                     throw new IllegalStateException("Column not found: " + column);
                 }
