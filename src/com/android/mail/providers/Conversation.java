363ae7c52d014ab67be68d510d9a977ac23f94f8
Paul Westbrook <pwestbro@google.com>
2012-09-27 10:57:14 -0700

    Move account cookie query into account object
    Move account cookie quert into the account object.  This will
    prevent looking up the account cookie from blocking the conversation
    list query
    
    Bug: 7244921
    Change-Id: I9761fe29bb05d9f29eb8cae640a749545362c770

--- src/com/android/mail/providers/Conversation.java
+++ src/com/android/mail/providers/Conversation.java
@@ -137,10 +137,6 @@ public class Conversation implements Parcelable {
      */
     public Uri conversationBaseUri;
     /**
-     * @see UIProvider.ConversationColumns#CONVERSATION_COOKIE
-     */
-    public String conversationCookie;
-    /**
      * @see UIProvider.ConversationColumns#REMOTE
      */
     public boolean isRemote;
@@ -200,7 +196,6 @@ public class Conversation implements Parcelable {
         dest.writeParcelable(accountUri, 0);
         dest.writeString(ConversationInfo.toString(conversationInfo));
         dest.writeParcelable(conversationBaseUri, 0);
-        dest.writeString(conversationCookie);
         dest.writeInt(isRemote ? 1 : 0);
     }
 
@@ -231,7 +226,6 @@ public class Conversation implements Parcelable {
         localDeleteOnUpdate = false;
         conversationInfo = ConversationInfo.fromString(in.readString());
         conversationBaseUri = in.readParcelable(null);
-        conversationCookie = in.readString();
         isRemote = in.readInt() != 0;
     }
 
@@ -296,7 +290,6 @@ public class Conversation implements Parcelable {
                     cursor.getString(UIProvider.CONVERSATION_BASE_URI_COLUMN);
             conversationBaseUri = !TextUtils.isEmpty(conversationBase) ?
                     Uri.parse(conversationBase) : null;
-            conversationCookie = cursor.getString(UIProvider.CONVERSATION_COOKIE_COLUMN);
             if (conversationInfo == null) {
                 snippet = cursor.getString(UIProvider.CONVERSATION_SNIPPET_COLUMN);
                 senders = emptyIfNull(cursor.getString(UIProvider.CONVERSATION_SENDER_INFO_COLUMN));
@@ -315,7 +308,7 @@ public class Conversation implements Parcelable {
             int numMessages, int numDrafts, int sendingState, int priority, boolean read,
             boolean starred, String rawFolders, int convFlags, int personalLevel, boolean spam,
             boolean phishing, boolean muted, Uri accountUri, ConversationInfo conversationInfo,
-            Uri conversationBase, String conversationCookie, boolean isRemote) {
+            Uri conversationBase, boolean isRemote) {
 
         final Conversation conversation = new Conversation();
 
@@ -343,7 +336,6 @@ public class Conversation implements Parcelable {
         conversation.accountUri = accountUri;
         conversation.conversationInfo = conversationInfo;
         conversation.conversationBaseUri = conversationBase;
-        conversation.conversationCookie = conversationCookie;
         conversation.isRemote = isRemote;
         return conversation;
     }
