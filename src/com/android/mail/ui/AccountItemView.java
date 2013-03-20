b958701ff6a98a7d95930e61d91104bb5db3299a
Rohan Shah <shahrk@google.com>
2013-03-20 14:34:38 -0700

    Add and separate Account Item View
    Move account out of FolderItemView and simplify
    some of the key functionality (removing params
    and instance vars).
    
    Fix the multi-select bug as well by disabling
    state_activated for the account_item style.
    
    Changed the header as well to make the style
    different for currently selected account.
    
    Bug: 8440682
    Change-Id: I55c55e403bc64733b5924f95f423fd1097c4dc16

--- src/com/android/mail/ui/AccountItemView.java
+++ src/com/android/mail/ui/AccountItemView.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2012, Google Inc.
+ * Copyright (c) 2013, Google Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,6 +22,7 @@ import android.widget.TextView;
 
 import com.android.mail.providers.Account;
 import com.android.mail.providers.Folder;
+import com.android.mail.providers.UIProvider.FolderType;
 import com.android.mail.utils.LogTag;
 import com.android.mail.utils.LogUtils;
 import com.android.mail.utils.Utils;
@@ -41,7 +42,7 @@ import android.widget.RelativeLayout;
 /**
  * The view for each folder in the folder list.
  */
-public class FolderItemView extends RelativeLayout {
+public class AccountItemView extends RelativeLayout {
     private final String LOG_TAG = LogTag.getLogTag();
     // Static colors
     private static int NON_DROPPABLE_TARGET_TEXT_COLOR;
@@ -61,35 +62,17 @@ public class FolderItemView extends RelativeLayout {
     private TextView mFolderTextView;
     private TextView mUnreadCountTextView;
     private TextView mUnseenCountTextView;
-    private DropHandler mDropHandler;
     private ImageView mFolderParentIcon;
 
-    /**
-     * A delegate for a handler to handle a drop of an item.
-     */
-    public interface DropHandler {
-        /**
-         * Return whether or not the drag event is supported by the drop handler. The
-         *     {@code FolderItemView} will present appropriate visual affordances if the drag is
-         *     supported.
-         */
-        boolean supportsDrag(DragEvent event, Folder folder);
-
-        /**
-         * Handles a drop event, applying the appropriate logic.
-         */
-        void handleDrop(DragEvent event, Folder folder);
-    }
-
-    public FolderItemView(Context context) {
+    public AccountItemView(Context context) {
         super(context);
     }
 
-    public FolderItemView(Context context, AttributeSet attrs) {
+    public AccountItemView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
-    public FolderItemView(Context context, AttributeSet attrs, int defStyle) {
+    public AccountItemView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
     }
 
@@ -116,28 +99,29 @@ public class FolderItemView extends RelativeLayout {
         mFolderParentIcon = (ImageView) findViewById(R.id.folder_parent_icon);
     }
 
-    public void bind(Folder folder, DropHandler dropHandler) {
-        mFolder = folder;
-        mDropHandler = dropHandler;
-        mFolderTextView.setText(folder.name);
-        mFolderParentIcon.setVisibility(mFolder.hasChildren ? View.VISIBLE : View.GONE);
-        if (mFolder.isInbox() && mFolder.unseenCount > 0) {
-            mUnreadCountTextView.setVisibility(View.GONE);
-            setUnseenCount(mFolder.getBackgroundColor(Color.BLACK), mFolder.unseenCount);
-        } else {
-            mUnseenCountTextView.setVisibility(View.GONE);
-            setUnreadCount(Utils.getFolderUnreadDisplayCount(mFolder));
-        }
-    }
-
-    public void bind(Account account, DropHandler dropHandler, int count) {
+    public void bind(Account account, int count) {
         mFolder = null;
-        mDropHandler = dropHandler;
         mFolderTextView.setText(account.name);
         mFolderParentIcon.setVisibility(View.GONE);
         mUnreadCountTextView.setVisibility(View.GONE);
         setUnseenCount(Color.BLACK, 0);
         setUnreadCount(count);
+    }
+
+    /**
+     * Takes in true if current item view should be modified to look like
+     * the current account header. This should not get called with inactive or
+     * non-displayed accounts.
+     *
+     * @param isCurrentAccount true if account is active, false otherwise
+     */
+    public void setCurrentAccount(boolean isCurrentAccount) {
+        if(isCurrentAccount) {
+            mUnreadCountTextView.setVisibility(View.GONE);
+            mFolderTextView.setAllCaps(true);
+            mFolderTextView.setTextColor(R.color.folder_list_heading_text_color);
+            mFolderTextView.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+        }
     }
 
     /**
@@ -173,68 +157,6 @@ public class FolderItemView extends RelativeLayout {
     }
 
     private boolean isDroppableTarget(DragEvent event) {
-        return (mDropHandler != null && mDropHandler.supportsDrag(event, mFolder));
-    }
-
-    /**
-     * Handles the drag event.
-     *
-     * @param event the drag event to be handled
-     */
-    @Override
-    public boolean onDragEvent(DragEvent event) {
-        switch (event.getAction()) {
-            case DragEvent.ACTION_DRAG_STARTED:
-                // If this folder is not a drop target, dim the text.
-                if (!isDroppableTarget(event)) {
-                    // Make sure we update this at the time we drop on the target.
-                    mInitialFolderTextColor = mFolderTextView.getTextColors();
-                    mInitialUnreadCountTextColor = mUnreadCountTextView.getTextColors();
-                    mFolderTextView.setTextColor(NON_DROPPABLE_TARGET_TEXT_COLOR);
-                    mUnreadCountTextView.setTextColor(NON_DROPPABLE_TARGET_TEXT_COLOR);
-                }
-                // Set the background to a steady state background.
-                setBackgroundDrawable(DRAG_STEADY_STATE_BACKGROUND);
-                return true;
-
-            case DragEvent.ACTION_DRAG_ENTERED:
-                // Change background color to indicate this folder is the drop target.
-                if (isDroppableTarget(event)) {
-                    setBackgroundDrawable(DROPPABLE_HOVER_BACKGROUND);
-                    return true;
-                }
-                break;
-
-            case DragEvent.ACTION_DRAG_EXITED:
-                // If this is a droppable target, make sure that it is set back to steady state,
-                // when the drag leaves the view.
-                if (isDroppableTarget(event)) {
-                    setBackgroundDrawable(DRAG_STEADY_STATE_BACKGROUND);
-                    return true;
-                }
-                break;
-
-            case DragEvent.ACTION_DRAG_ENDED:
-                // Reset the text of the non draggable views back to the color it had been..
-                if (!isDroppableTarget(event)) {
-                    mFolderTextView.setTextColor(mInitialFolderTextColor);
-                    mUnreadCountTextView.setTextColor(mInitialUnreadCountTextColor);
-                }
-                // Restore the background of the view.
-                setBackgroundDrawable(mBackground);
-                return true;
-
-            case DragEvent.ACTION_DRAG_LOCATION:
-                return true;
-
-            case DragEvent.ACTION_DROP:
-                if (mDropHandler == null) {
-                    return false;
-                }
-
-                mDropHandler.handleDrop(event, mFolder);
-                return true;
-        }
         return false;
     }
 }
