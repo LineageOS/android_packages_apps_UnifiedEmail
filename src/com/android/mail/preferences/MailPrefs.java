1c5fed23e5ec51f04557677c6a9353374dd44d4c
Rohan Shah <shahrk@google.com>
2013-03-28 09:50:52 -0700

    Added account recents support
    RecentAccountsList will now maintain the ordering of
    accounts from least to most recently used.
    
    Will be adding to drawer once most of the refactoring
    is done.
    
    Bug: 8449354
    Change-Id: I56e6c401bb3cfeaaa06d61e4c2e048f21c107dd8

--- src/com/android/mail/preferences/MailPrefs.java
+++ src/com/android/mail/preferences/MailPrefs.java
@@ -53,7 +53,10 @@ public final class MailPrefs extends VersionedPrefs {
          * A boolean that, if <code>true</code>, means we should default all replies to "reply all"
          */
         public static final String DEFAULT_REPLY_ALL = "default-reply-all";
-
+        /**
+         * A preference for storing most recently used accounts (ordered list or URIs)
+         */
+        public static final String RECENT_ACCOUNTS = "recent-account-uris";
         public static final String CONVERSATION_LIST_SWIPE_ACTION =
                 "conversation-list-swipe-action";
 
@@ -189,6 +192,24 @@ public final class MailPrefs extends VersionedPrefs {
     public void setDefaultReplyAll(final boolean replyAll) {
         getEditor().putBoolean(PreferenceKeys.DEFAULT_REPLY_ALL, replyAll).apply();
         MailIntentService.broadcastBackupDataChanged(getContext());
+    }
+
+    /**
+     * Return a list of URIs corresponding to the most recently used accounts
+     *
+     * @return uris of accounts from least recently used to most recently used
+     */
+    public String getRecentAccountUris() {
+        return getSharedPreferences().getString(PreferenceKeys.RECENT_ACCOUNTS,
+                "");
+    }
+
+    /**
+     * Take in an ArrayList of account URIs that are in order from least recently
+     * used to most recently used, and save it as prefs.
+     */
+    public void setRecentAccountUris(String accountUris) {
+        getEditor().putString(PreferenceKeys.RECENT_ACCOUNTS, accountUris).apply();
     }
 
     /**
