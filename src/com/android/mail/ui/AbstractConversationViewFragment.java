b8e6ea1f56b21c444ff7aec5b9165f011a6dd8a6
Alice Yang <alice@google.com>
2013-05-08 04:43:10 +0000

    Revert "Disable transforms temporarily"
    This reverts commit ebfbfbd4a429a0ae720735cb988c9f48f646999a
    
    Change-Id: I806a76d0d0887db3f867748f5ffc33a6b1cd162d

--- src/com/android/mail/ui/AbstractConversationViewFragment.java
+++ src/com/android/mail/ui/AbstractConversationViewFragment.java
@@ -862,8 +862,6 @@ public abstract class AbstractConversationViewFragment extends Fragment implemen
      * @return {@code true} if the conversation should be transformed. {@code false}, otherwise.
      */
     public boolean shouldApplyTransforms() {
-        // Temporarily disable transforms to debug 8835996
-        return false;
-//        return !mHasConversationTransformBeenReverted;
+        return !mHasConversationTransformBeenReverted;
     }
 }
