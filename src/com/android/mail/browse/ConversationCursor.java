c9970641a82ed7f25deae33899a41a8669d4b445
Alon Albert <aalbert@google.com>
2013-11-06 11:38:25 -0800

    Proper getNotificationUri()
    Copied getNotificationUri() from mr11 where it was done properly.
    
    Bug: 11533662
    Change-Id: I81e200416d5daddac897e6ae9ebdc68abf33e412

--- src/com/android/mail/browse/ConversationCursor.java
+++ src/com/android/mail/browse/ConversationCursor.java
@@ -1946,13 +1946,17 @@ public final class ConversationCursor implements Cursor, ConversationCursorOpera
     }
 
     @Override
-    public void setNotificationUri(ContentResolver cr, Uri uri) {
-        throw new UnsupportedOperationException();
+    public Uri getNotificationUri() {
+        if (mUnderlyingCursor == null) {
+            return null;
+        } else {
+            return mUnderlyingCursor.getNotificationUri();
+        }
     }
 
     @Override
-    public Uri getNotificationUri() {
-        return null;
+    public void setNotificationUri(ContentResolver cr, Uri uri) {
+        throw new UnsupportedOperationException();
     }
 
     @Override
