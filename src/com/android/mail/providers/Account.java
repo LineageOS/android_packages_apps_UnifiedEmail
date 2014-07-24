8d7d0b7dda368390c04dc6fd8156cf7a6ed72d6e
Ray Chen <raychen@google.com>
2014-07-24 12:20:43 +0200

    Use getAccountId for AccountPreferences and FolderPreferences.
    Change-Id: I7dab0aa619a9372b38c2336af9ab19df1629da84

--- src/com/android/mail/providers/Account.java
+++ src/com/android/mail/providers/Account.java
@@ -850,7 +850,7 @@ public class Account implements Parcelable {
     /**
      * The account id is an unique id to represent this account.
      */
-    public String getAccountId(Context context) {
+    public String getAccountId() {
         LogUtils.d(LogUtils.TAG, "getAccountId = %s for email %s", accountId, accountManagerName);
         return accountId;
     }
