95602ad397e3b75acf81042987133ce786fed38f
Jorge Ruesga <jorge@ruesga.com>
2015-03-01 22:02:55 +0100

    unified email: suggested contacts
    This change adds support for suggested contacts (email addresses not in the contact
    provider and received via email).
    
    Change-Id: I4010d0b6cf8bbb98308c7fc13f63e9d859a87d43
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

---
+++ src/com/android/mail/ui/RecipientEditTextViewWithSuggestedContacts.java
@@ -0,0 +1,14 @@
+package com.android.mail.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.android.ex.chips.RecipientEditTextView;
+
+public class RecipientEditTextViewWithSuggestedContacts extends RecipientEditTextView {
+
+    public RecipientEditTextViewWithSuggestedContacts(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
