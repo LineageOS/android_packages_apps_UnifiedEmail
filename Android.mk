3c04d424d9da3dcb809f8f6238a11001da36c1e2
Kenshin <foufou33@gmail.com>
2015-03-12 13:54:52 -0700

    UnifiedEmail: Fix build
    Change-Id: I180829783123498b1c6a06b0d877e9ce451429b0

--- Android.mk
+++ Android.mk
@@ -43,6 +43,7 @@ LOCAL_STATIC_JAVA_LIBRARIES += android-support-v13
 LOCAL_STATIC_JAVA_LIBRARIES += android-opt-bitmap
 LOCAL_STATIC_JAVA_LIBRARIES += android-opt-datetimepicker
 LOCAL_STATIC_JAVA_LIBRARIES += owasp-html-sanitizer
+LOCAL_STATIC_JAVA_LIBRARIES += com.android.emailcommon
 
 LOCAL_SDK_VERSION := current
 
@@ -50,7 +51,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, $(src_dirs)) \
         $(call all-logtags-files-under, $(src_dirs))
 LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, $(res_dirs))
 LOCAL_AAPT_FLAGS := --auto-add-overlay
-LOCAL_AAPT_FLAGS += --extra-packages com.android.ex.chips:com.android.ex.photo:android.support.v7.appcompat:android.support.v7.gridlayout:com.android.bitmap:com.android.datetimepicker
+LOCAL_AAPT_FLAGS += --extra-packages com.android.ex.chips:com.android.ex.photo:android.support.v7.appcompat:android.support.v7.gridlayout:com.android.bitmap:com.android.datetimepicker:com.android.emailcommon
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
