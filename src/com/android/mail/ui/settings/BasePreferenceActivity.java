ddd70286616b55af96dd27f1e49e0bed1fc90b37
Danny Baumann <dannybaumann@web.de>
2015-10-15 14:09:06 +0200

    Add an ActionBar to the mail app's PreferenceActivity.
    Change-Id: Ie068bcb3e0f7f92fc4dc799f5d8913731d5296ba

---
+++ src/com/android/mail/ui/settings/BasePreferenceActivity.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.mail.ui.settings;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toolbar;
+
+import com.android.mail.R;
+
+public abstract class BasePreferenceActivity extends PreferenceActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setActionBar(toolbar);
+    }
+
+    @Override
+    public void setContentView(int layoutResID) {
+        final LayoutInflater inflater = getLayoutInflater();
+        View contentView = inflater.inflate(R.layout.preference_activity, null);
+
+        ViewGroup contentWrapper = (ViewGroup) contentView.findViewById(android.R.id.content);
+        inflater.inflate(layoutResID, contentWrapper);
+
+        getWindow().setContentView(contentView);
+    }
+
+}
