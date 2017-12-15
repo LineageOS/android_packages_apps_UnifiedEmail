5e14277b98a75e5ba209fe27de8eeee5d0a48e5f
Paul Duffin <paulduffin@google.com>
2017-12-15 07:22:38 +0000

    Add android.test.base/stubs dependency
    In preparation for the removal of the non-junit classes in the
    android.test.base library from the android.jar this adds a dependency
    on android.test.base/stubs to ensure this code will continue to
    compile.
    
    The following change descriptions were generated automatically and so
    may be a little repetitive. They are provided to give the reviewer
    enough information to check the comments match what has actually been
    changed and check the reasoning behind the changes.
    
    * tests/Android.mk
        Added 'android.test.base.stubs' to LOCAL_JAVA_LIBRARIES because
        UnifiedEmailTests's will need them in order to compile once its
        classes are removed from the current SDK on which it currently
        depends.
    
    Bug: 30188076
    Test: make dist
    Change-Id: Ie7d1b8536442153b97427dc7277e297188e23165

--- tests/Android.mk
+++ tests/Android.mk
@@ -33,7 +33,11 @@ LOCAL_SRC_FILES := $(call all-java-files-under, $(src_dirs))
 LOCAL_AAPT_FLAGS := --auto-add-overlay
 LOCAL_STATIC_JAVA_LIBRARIES := android-support-test
 
-LOCAL_JAVA_LIBRARIES := android.test.mock.stubs android.test.runner.stubs
+LOCAL_JAVA_LIBRARIES := \
+    android.test.mock.stubs \
+    android.test.runner.stubs \
+    android.test.base.stubs \
+
 
 include $(BUILD_PACKAGE)
 
