ec01df7f1bce0516b9c1c1945dd603beb9037aee
Ekin Oguz <ekinoguz@google.com>
2018-01-29 13:07:34 -0800

    Disallow attaching files from our own EmailAttachmentProvider.
    This is to backport a security fix reported in b/71814449 and
    b/72569023. Fix is using the same approach as b/27308057, which is to
    prevent Compose from accepting URIs with our own email attachment
    provider.
    
    Bug: b/71814449
    Change-Id: Idcc002b94bcea913383b54bcf1dc5c3e1d254ded
    (cherry picked from commit 44a5db2dc0ace4d360699c11c27d5c10b43ad628)
    (cherry picked from commit e00598532bbfc8618b7c051cbf6bd15491f61f27)

--- src/com/android/mail/compose/ComposeActivity.java
+++ src/com/android/mail/compose/ComposeActivity.java
@@ -1946,6 +1946,15 @@ public class ComposeActivity extends ActionBarActivity
     }
 
     /**
+     * @return the authority of EmailAttachmentProvider for this app. should be overridden in
+     * concrete app implementations. can't be known here because this project doesn't know about
+     * that sort of thing.
+     */
+    protected String getEmailAttachmentProviderAuthority() {
+        throw new UnsupportedOperationException("unimplemented, EmailAttachmentProvider unknown");
+    }
+
+    /**
      * Helper function to handle a list of uris to attach.
      * @return the total size of all successfully attached files.
      */
@@ -1966,7 +1975,9 @@ public class ComposeActivity extends ActionBarActivity
                         }
                     } else if (ContentResolver.SCHEME_CONTENT.equals(uri.getScheme())) {
                         // disallow attachments from our own EmailProvider (b/27308057)
-                        if (getEmailProviderAuthority().equals(uri.getAuthority())) {
+                        if (getEmailProviderAuthority().equals(uri.getAuthority())
+                                || getEmailAttachmentProviderAuthority().equals(
+                                        uri.getAuthority())) {
                             showErrorToast(getString(R.string.attachment_permission_denied));
                             Analytics.getInstance().sendEvent(ANALYTICS_CATEGORY_ERRORS,
                                     "send_intent_attachment", "email_provider", 0);
