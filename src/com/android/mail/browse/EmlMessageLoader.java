b64f24986ec886046d56f933d59ffde26976d6b5
Raman Tenneti <rtenneti@google.com>
2019-06-25 21:46:16 +0000

    Revert "AOSP/UnifiedEmail - Bumped the targetSdkVersion to 28 (maximum version). Used JobIntentservice API calls to replace context.startService."
    This reverts commit c851a0a2fa18cf76f4fe5eb53ce489f051fb7019.
    
    Reason for revert: No notification when new email arrived
    Bug: 132923014
    
    Cherry picking - https://android-review.googlesource.com/c/platform/packages/apps/UnifiedEmail/+/990099
    
    Test: manual - Ran the following tests on Pixel phone. Tested the email UI for notification.
    
    $ make -j 40
    $ make EmailTests -j
    $ adb install -r out/target/product/marlin/system/product/app/Email/Email.apk
    $ adb install -r out/target/product/marlin/testcases/EmailTests/arm64/EmailTests.apk
    
    $ adb shell am instrument -w com.android.email.tests
      Time: 2.986
      OK (157 tests)
    
    Change-Id: I010565b2f8616195f397db4473b3a34fc8b150b5
    Merged-In: I010565b2f8616195f397db4473b3a34fc8b150b5

--- src/com/android/mail/browse/EmlMessageLoader.java
+++ src/com/android/mail/browse/EmlMessageLoader.java
@@ -105,7 +105,7 @@ public class EmlMessageLoader extends MailAsyncTaskLoader<ConversationMessage> {
             intent.setClass(getContext(), EmlTempFileDeletionService.class);
             intent.setData(message.attachmentListUri);
 
-            EmlTempFileDeletionService.enqueueWork(getContext(), intent);
+            getContext().startService(intent);
         }
     }
 }
